9c1930a401c50c6f982525508f643fa3b47c2f22
Victor <vypl.snf19@uea.edu.br>
2020-03-27 19:24:41 -0400

    routes file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req,res) => {
-    return res.json({msg:"Hello World"});
-});
 
 app.listen(3333);
