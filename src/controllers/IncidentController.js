0e3b7fdaf3a7e78b450c699d9e6223d9cd72d9fc
Victor <vypl.snf19@uea.edu.br>
2020-03-28 19:08:43 -0400

    Incident Controller

---
+++ src/controllers/IncidentController.js
@@ -0,0 +1,23 @@
+const connection = require('../database/connection');
+const crypto = require('crypto');
+
+module.exports = {
+    async index(req,res) {
+        const incidents = await connection('incidents').select('*');
+    
+        return res.json(incidents);
+    },
+    async create(req,res) {
+        const ong_id = req.headers.authorization;
+        const { title, description, value } = req.body;
+    
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id
+        });
+    
+        return res.json({ id });
+    }
+}
